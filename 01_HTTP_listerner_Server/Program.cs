ffbfb2213e3506dca75e5df1116c5df2b376c641
Pavlo Marchuk <Pavlo Marchuk@DESKTOP-MQBICKJ>
2017-06-27 13:57:19 +0300

    HTTP_01

--- 01_HTTP_listerner_Server/Program.cs
+++ 01_HTTP_listerner_Server/Program.cs
@@ -1,15 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace _01_HTTP_listerner_Server
 {
+	
 	class Program
 	{
+		//static Server server;
+		static Thread serverThread;
 		static void Main(string[] args)
 		{
+			HttpListener listerner = new HttpListener();
+			listerner.Prefixes.Add("http://localhost:8888/connection/");
+			listerner.Start();
+
+			Console.WriteLine("Server Started!!!!!");
+			try
+			{
+				// while
+				while(true)
+				{
+					HttpListenerContext context = listerner.GetContext();
+					// запоточити все далі, передавши контекст
+					Server_Http server = new Server_Http(context);
+					serverThread = new Thread(new ThreadStart(server.Listen));
+					serverThread.Start();
+				}
+			}
+			//HttpListenerRequest request = context.Request;
+			//foreach(var item in request.QueryString)
+			//{
+			//	Console.WriteLine();
+			//}
+			//string name = "unnown";
+			//if(request.QueryString["name"] != null)
+			//{
+			//	 name =  request.QueryString["name"];
+			//	Console.WriteLine("name=" + name);
+			//}
+			//if(request.QueryString["age"] != null)
+			//	Console.WriteLine("age=" + request.QueryString["age"]);
+			//HttpListenerResponse response = context.Response;
+
+			//string html = "<html><head> <meta charset='UTF - 8'> <body> Hello! "+ name + " </body > </head></html>";
+
+			//byte[]  data = Encoding.UTF8.GetBytes(html);
+			//response.ContentLength64 = data.Length;
+
+			//Stream writer = response.OutputStream;
+			//writer.Write(data, 0, data.Length);
+
+			//Console.Read();
+			//writer.Close();
+			////////////////////////////
+			catch(Exception)
+			{
+				listerner.Stop();
+			}
+			
 		}
 	}
 }
---
+++ 01_HTTP_listerner_Server/Server_Http.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_HTTP_listerner_Server
+{
+	class Server_Http
+	{
+		private HttpListenerContext context;
+		public Server_Http(HttpListenerContext context_)
+		{
+			context = context_;
+		}
+		public  void Listen()
+		{
+			
+			HttpListenerRequest request = context.Request;
+			foreach(var item in request.QueryString)
+			{
+				Console.WriteLine();
+			}
+			string name = "unnown";
+			if(request.QueryString["name"] != null)
+			{
+				name = request.QueryString["name"];
+				Console.WriteLine("name=" + name);
+			}
+
+			if(request.QueryString["age"] != null)
+				Console.WriteLine("age=" + request.QueryString["age"]);
+			HttpListenerResponse response = context.Response;
+
+			string html = "<html><head> <meta charset='UTF - 8'> <body> Hello! "+ name + " age= " +request.QueryString["age"] + " </body > </head></html>";
+
+			byte[]  data;
+			Stream writer;
+			if(Int32.Parse(request.QueryString["age"]) < 21)
+			{
+				data = Young();
+			}
+			else
+			{
+				data = Encoding.UTF8.GetBytes(html);
+			}
+			response.ContentLength64 = data.Length;
+
+			writer = response.OutputStream;
+			writer.Write(data, 0, data.Length);
+
+			Console.Read();
+			writer.Close();		
+		}
+
+		private byte[] Young()
+		{
+			string html = "<html><head> <meta charset='UTF - 8'> <body>too Young! </body > </head></html>";
+			return Encoding.UTF8.GetBytes(html);
+		}
+
+	}
+}
