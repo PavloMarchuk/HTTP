a3a82b98ab8bcc880b29e1a922cde08fa35cb20a
Pavlo Marchuk <Pavlo Marchuk@DESKTOP-MQBICKJ>
2017-06-29 15:55:23 +0300

    03_Server2 потім 02_HTTP_client

--- 02_HTTP_client/Program.cs
+++ 02_HTTP_client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -11,6 +12,12 @@ namespace _02_HTTP_client
 	{
 		static void Main(string[] args)
 		{
+			GetRequest("http://www.google.com");			
+			Console.WriteLine("\n\nConsole.ReadKey()  =============================================================================");
+			Console.ReadKey();
+			//Console.WriteLine("\n\nConsole.ReadKey()  =============================================================================");
+			PostRequest("http://localhost:8888/connection");
+			Console.ReadKey();
 		}
 
 		async static void GetRequest ( string uri)
@@ -22,14 +29,45 @@ namespace _02_HTTP_client
 					using(HttpContent content = respons.Content)
 					{
 						string message =await content.ReadAsStringAsync();
+						Console.WriteLine(message);
+						Console.WriteLine("HEDER:");
+						Console.WriteLine(content.Headers);
 						Console.WriteLine();
+						Console.WriteLine(message.Length);
 					}
 				}
 			}
 		}
 
 		async static void PostRequest(string uri)
-		{ }
+		{
+			IEnumerable<KeyValuePair<String, string>> parameters = new List<KeyValuePair<String, string>>()
+			{
+				new KeyValuePair<string, string>("name", "igor"),
+				new KeyValuePair<string, string>("age", "18")
+			};
+			HttpContent contentQery = new FormUrlEncodedContent(parameters);
 
+			using(HttpClient client = new HttpClient())
+			{
+				using(HttpResponseMessage respons = await client.PostAsync(uri, contentQery))
+				{
+					Console.WriteLine(respons.StatusCode);
+					if((int)respons.StatusCode == 404)
+					{
+						Console.WriteLine("СТОРІНКА НЕ ЗНАЙДЕНА (404)");
+					}
+					using(HttpContent content = respons.Content)
+					{
+						string message = await content.ReadAsStringAsync();
+						Console.WriteLine(message);
+						Console.WriteLine("HEDER:");
+						Console.WriteLine(content.Headers);
+						Console.WriteLine();
+						Console.WriteLine(message.Length);						
+					}
+				}
+			}
+		}
 	}
 }
---
+++ 03_Server2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _03_Server2
+{
+	class Program
+	{		
+		static void Main(string[] args)
+		{
+			HttpListener listerner = new HttpListener();
+			listerner.Prefixes.Add("http://localhost:8888/connection/");
+
+			listerner.Start();
+
+			Console.WriteLine("Server Started!!!!!");
+
+			HttpListenerContext context = listerner.GetContext();
+			HttpListenerRequest request = context.Request;
+
+			string text;
+			//string name = "unknown";
+
+			Dictionary<string, string> parametrs= new Dictionary<string, string>();
+
+			if(request.ContentLength64 > 0) //параметри через POST
+			{
+				using(var reader = new StreamReader(request.InputStream, request.ContentEncoding))
+				{
+					text = reader.ReadToEnd();
+
+					string[] parametersPairs = text.Split('&');
+					for(int i = 0; i < parametersPairs.Length; i++)
+					{
+						string [] pair = parametersPairs[i].Split('=');
+						parametrs.Add(pair[0], pair[1]);						
+					}
+				}
+
+			}
+			else
+			if(request.QueryString.Count > 0) //параметри через URL
+			{
+				string [] keys = request.QueryString.AllKeys;
+				for(int i = 0; i < keys.Length; i++)
+				{
+					parametrs.Add(keys[i], request.QueryString[keys[i]]);
+				}
+			}
+			else// дефолтне значення
+			{
+				parametrs.Add("name", "Guest");
+				// home page
+			}
+			
+			HttpListenerResponse response = context.Response;
+
+			string html = "<html><head> <meta charset='UTF - 8'> <body> Hello! " + parametrs["name"] + " </body > </head></html>";
+
+			byte[] data = Encoding.UTF8.GetBytes(html);
+			response.ContentLength64 = data.Length;
+
+			Stream writer = response.OutputStream;
+			writer.Write(data, 0, data.Length);
+
+			Console.Read();
+			writer.Close();
+			////////////////////////////
+			listerner.Stop();
+		}
+	}
+}
