49a050087aee51a30cf5502239a3a1fa18e3d838
Pavlo Marchuk <Pavlo Marchuk@DESKTOP-MQBICKJ>
2017-06-29 09:40:45 +0300

    HTTP_client_01

---
+++ 02_HTTP_client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_HTTP_client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		async static void GetRequest ( string uri)
+		{
+			using(HttpClient client = new HttpClient())
+			{
+				using(HttpResponseMessage respons = await client.GetAsync(uri))
+				{
+					using(HttpContent content = respons.Content)
+					{
+						string message =await content.ReadAsStringAsync();
+						Console.WriteLine();
+					}
+				}
+			}
+		}
+
+		async static void PostRequest(string uri)
+		{ }
+
+	}
+}
